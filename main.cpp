bd25243fc9495d00d29d8ea698d28b3c4e3e80f0
prostolyalya <prostolyalya@outlook.com>
2020-07-06 13:54:23 +0300

    final

--- main.cpp
+++ main.cpp
@@ -1,37 +1,42 @@
-#include <iostream>
 #include <stdio.h>
-#include "LibNumToHex/header/lib.h"
+
+#include <iostream>
+#include <string>
+
+#include "LibConvert/lib.h"
 
 using namespace std;
 
-string converts()
+int main( )
 {
-    FILE *f = fopen("result", "rb");
-    fseek(f, 0, SEEK_END);
-    int size = ftell(f);
-    rewind(f);
-    char * dataC = new char[size];
-
-    fread(dataC, size, size, f);
-    fclose(f);
-    string data = dataC;
-    int res = libConvert::convertHexToDec(data);
-    delete[] dataC;
-    return to_string(res);
-}
-
-int main()
-{
-//    cout << converts() << endl;
-    cout << "Input number ";
-    int num = 0;
-    cin >> num;
-    string res = libConvert::convertDecToHex(num);
-    FILE *f = fopen("result", "ab+");
-    fwrite(res.c_str(), res.length(), res.length(), f);
-    fclose(f);
-
+    cout << "Read or write (r/w) ";
+    char input;
+    cin >> input;
+    if ( input == 'w' )
+    {
+        cout << "Input number ";
+        int num = 0;
+        cin >> num;
+        string res = libConvert::convertDecToHex( num );
+        FILE* f = fopen( "result", "wb" );
+        fwrite( res.c_str( ), res.length( ), res.length( ), f );
+        fclose( f );
+    }
+    else if ( input == 'r' )
+    {
+        FILE* f = fopen( "result", "rb" );
+        fseek( f, 0, SEEK_END );
+        int size = ftell( f );
+        rewind( f );
+        char* dataC = new char[ size ];
+        fread( dataC, size, size, f );
+        fclose( f );
+        string data = dataC;
+        int res = libConvert::convertHexToDec( data );
+        delete[] dataC;
+        cout << res << endl;
+    }
+    else
+        cout << "Incorrect, close" << endl;
     return 0;
 }
-
-
