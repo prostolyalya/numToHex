3af7415ad37cc52cd08bedcd5c43c1bd3063fa75
prostolyalya <prostolyalya@outlook.com>
2020-07-08 10:23:29 +0300

    formatting & check found gtest package

--- test/libTest.cpp
+++ test/libTest.cpp
@@ -1,7 +1,8 @@
-#include <gtest/gtest.h>
 #include <LibConvert/lib.h>
+#include <gtest/gtest.h>
 
-TEST(libTest, test1) {
-    EXPECT_EQ (libConvert::convertDecToHex(12), "0x12");
-    EXPECT_EQ (libConvert::convertHexToDec("12"), 18);
+TEST( libTest, test1 )
+{
+    EXPECT_EQ( libConvert::convertDecToHex( 12 ), "0x12" );
+    EXPECT_EQ( libConvert::convertHexToDec( "12" ), 18 );
 }
